63c3f84cc2a5554ded2eb3576da96bd3ad402027
LordGudzo <lordgudzo@ukr.net>
2023-04-17 13:56:00 +0300

    question for learning

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 //coonect to mongoDB
 import mongoose from "mongoose";
 mongoose
-    .connect("mongodb+srv://LordGudzo:w6@cluster0.ynaurch.mongodb.net/?retryWrites=true&w=majority")
+    .connect("mongodb+srv://LordGudzo:w6@cluster0.ynaurch.mongodb.net/blog?retryWrites=true&w=majority")
     .then(() => console.log("DB ok"))
     .catch((err) => console.log('DB error', err));
 //validation block
@@ -23,27 +23,44 @@ app.use(express.json());
 
 
 app.post("/auth/register", registerValidation, async(req, res) => {
-    //contains all errors after validaton
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors.array());
+    try{
+            //contains all errors after validaton
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json(errors.array());
+        }
+
+        //encryption password
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id
+        }, 
+        "secret123",
+        {
+          expiresIn: "30d"  
+        })
+
+        res.json({
+           ... user._doc,
+           token
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json("not succesfull registration")
     }
-
-    //encryption password
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash
-    });
-
-    const user = await doc.save();
-
-    res.json({user});
+    
 });
 
 
